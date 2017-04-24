9f76f1e0d783dcfaba7304f21111cffa1f5d7ab0
koyee <koyeelin@163.com>
2017-04-24 18:13:20 +0800

    项目的第二天：1了解设计图2完成与后台的交接3熟悉后台给出的api4开始划分组件

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,29 +1,26 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <div class="header">
+      I am header!
+    </div>
+    <div class="tab">
+      I am tab
+    </div>
+    <div class="content">
+      I am content  
+    </div>	
   </div>
-</template>
+</template> 
 
 <script>
-// ctrl + alt + l 格式代码
-import Hello from './components/Hello'
+  import header from './components/header/header.vue';
 
-export default {
-  name: 'app',
-  components: {
-    Hello
-  }
-}
+  export default {
+    components: {
+      header: header
+    }
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
